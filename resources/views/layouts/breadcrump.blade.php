1fb65c6652b222fb8e98d5c2b8bb164e8b206f8e
vinothini.k@mindnotix.in <vinothini.k@mindnotix.in>
2020-09-12 10:06:33 +0530

    Breadcrumb

--- resources/views/layouts/breadcrump.blade.php
+++ resources/views/layouts/breadcrump.blade.php
@@ -1,10 +1,12 @@
 
 <?php  
-    
+
 $route_name = null;
 $name = null;
 $home = $route[0];
+
 if(isset($route[1])){
+  
   $second = $route[1];
   if ( $second === 'index' || $second === 'edit' || $second === 'show' || $second === 'create') {
     
@@ -16,6 +18,7 @@ if(isset($route[1])){
     $route_name = ucfirst($route[0]).' '. ucfirst($name);
   }
 }
+}
 ?>
 
 
@@ -26,7 +29,7 @@ if(isset($route[1])){
     <ol class="breadcrumb p-0 mb-0">
       <li class="breadcrumb-item"><a href="{{route('store.index')}}"><i class="bx bx-home-alt"></i></a>
       </li>
-    <li class="breadcrumb-item"><a href="{{ route($home) }}">{{ucfirst($route[0])}}</a>
+    <li class="breadcrumb-item"><a href="#">{{ucfirst($route[0])}}</a>
       </li>
       <li class="breadcrumb-item active">{{$route_name}}
       </li>
