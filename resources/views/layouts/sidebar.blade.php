bc9771669ad6747066ca19f9734339d4c9e162d8
vinothini.k@mindnotix.in <vinothini.k@mindnotix.in>
2020-10-02 17:20:50 +0530

    .

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -50,8 +50,10 @@
       <div class="navbar-header">
         <ul class="nav navbar-nav flex-row">
           <li class="nav-item mr-auto"><a class="navbar-brand" href="index.html">
-              <div class="brand-logo"><img class="logo" src="{{ asset('assets/app-assets/images/logo/logo.png') }}"/></div>
-              <h2 class="brand-text mb-0">Flash</h2></a></li>
+              <div class="">
+                {{-- <img class="logo" src="{{ asset('assets/app-assets/images/logo/logo.png') }}"/> --}}
+              </div>
+              <h2 class="brand-text mb-0">NammaArea51</h2></a></li>
           <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="bx bx-x d-block d-xl-none font-medium-4 primary"></i><i class="toggle-icon bx bx-disc font-medium-4 d-none d-xl-block primary" data-ticon="bx-disc"></i></a></li>
         </ul>
       </div>
