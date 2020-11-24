2dc2a1aaceee5a7c3e70225fdf1fe6aa9379c9ca
vinothini.k@mindnotix.in <vinothini.k@mindnotix.in>
2020-11-24 15:08:43 +0530

    .

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\OrderDetails;
 use App\InvoiceDetailsData;
-use DB;
+use DB, Carbon\Carbon;
 class DashboardController extends Controller
 {
 
 
-    public function index(Request $request, $year){
+    public function index(Request $request, $year, $month){
         $grandtotal = NULL;
         $data['delivered_orders'] = OrderDetails::where('order_delivery_status_id', 4)->count();
         $invoice_details = InvoiceDetailsData::where('order_delivery_status_id', 4)->get();
@@ -42,20 +42,36 @@ class DashboardController extends Controller
                                             ->where('order_details.order_delivery_status_id', 4)
                                             ->pluck('order_master.final_paid_amount');
         $data['monthly_sales'] = [];
-        for ($month = 1; $month < 13 ; $month++) { 
+        for ($i = 1; $i < 13 ; $i++) { 
             $data['monthly'] = DB::table('invoice_details_data')
                                             ->join('order_details', 'order_details.order_details_id', '=', 'invoice_details_data.order_details_id')
                                             ->join('order_master', 'order_master.order_id', '=', 'order_details.order_id')
                                             ->where('order_details.order_delivery_status_id', 4)
                                             ->whereYear('order_details.create_date_time', $year)
-                                            ->whereMonth('order_details.create_date_time', $month)
+                                            ->whereMonth('order_details.create_date_time', $i)
                                             ->pluck('order_master.final_paid_amount')->toArray();
                         array_push($data['monthly_sales'], array_sum($data['monthly']));
         }
-        
+        $data['weekly_sales'] = [];
+
+        $weeks = [1 => 8, 8 => 15, 15=> 22, 22 => 31];
+        foreach ($weeks as $key => $week) {
+            $from = $year."-".$month."-".$key;
+            $to = $year."-".$month."-".$week;
+            $data['weekly'] = DB::table('invoice_details_data')
+                                            ->join('order_details', 'order_details.order_details_id', '=', 'invoice_details_data.order_details_id')
+                                            ->join('order_master', 'order_master.order_id', '=', 'order_details.order_id')
+                                            ->where('order_details.order_delivery_status_id', 4)
+                                            ->whereYear('order_details.create_date_time', $year)
+                                            ->whereMonth('order_details.create_date_time', $month)
+                                            ->whereBetween('order_details.create_date_time', [$from, $to])
+                                            ->pluck('order_master.final_paid_amount')->toArray();
+                                            // ->get();
+                        array_push($data['weekly_sales'], array_sum($data['weekly']));
+        }        
                                             
         
-        // dd($data['monthly_sales']);
+        // dd($data['weekly_sales']);
         $data['total_customers'] = DB::table('customer_master')->count();
         $data['total_earnings'] = array_sum($data['earnings_delivered']->toArray());
         // dd($grandtotal);
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -8,124 +8,7 @@
     <!-- Scroll - horizontal and vertical table -->
     <h5><b>Dashboard</b></h5> <br />
     <section id="horizontal-vertical">
-        <div class="content-body"><!-- Dashboard Ecommerce Starts -->
-            {{-- <section id="dashboard-ecommerce">
-                <div class="row">
-                    <!-- Greetings Content Starts -->
-
-                    <!-- Multi Radial Chart Starts -->
-                    <div class="col-xl-4 col-12 dashboard-users">
-                        <div class="row  ">
-                            <!-- Statistics Cards Starts -->
-                            <div class="col-12">
-                                <div class="row">
-                                    <div class="col-sm-6 col-12 dashboard-users-success">
-                                        <div class="card text-center">
-                                            <div class="card-content">
-                                                <div class="card-body py-1">
-                                                    <div class="badge-circle badge-circle-lg badge-circle-light-success mx-auto mb-50">
-                                                        <i class="bx bx-briefcase-alt font-medium-5"></i>
-                                                    </div>
-                                                    <div class="text-muted">Total Orders Delivered</div>
-                                                    <h3 class="mb-0">{{$delivered_orders}}</h3>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-6 col-12 dashboard-users-danger">
-                                        <div class="card text-center">
-                                            <div class="card-content">
-                                                <div class="card-body py-1">
-                                                    <div class="badge-circle badge-circle-lg badge-circle-light-danger mx-auto mb-50">
-                                                        <i class="bx bx-user font-medium-5"></i>
-                                                    </div>
-                                                    <div class="text-muted line-ellipsis">Placed Orders</div>
-                                                    <h3 class="mb-0">45.6k</h3>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Revenue Growth Chart Starts -->
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xl-8 col-12 dashboard-order-summary" id="order_summary">
-                        <div class="card">
-                            <div class="row">
-                                <!-- Order Summary Starts -->
-                                <div class="col-md-8 col-12 order-summary border-right pr-md-0" onload="order_graph()">
-                                    <div class="card mb-0">
-                                        <div class="card-header d-flex justify-content-between align-items-center">
-                                            <h4 class="card-title">Order Summary</h4>
-                                            <div class="d-flex">
-                                                <button type="button" class="btn btn-sm btn-light-primary mr-1">Month</button>
-                                                <button type="button" class="btn btn-sm btn-primary glow">Week</button>
-                                            </div>
-                                        </div>
-                                        <div class="card-content">
-                                            <div class="card-body p-0">
-                                                <div id="order-summary-chart">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- Sales History Starts -->
-                                <div class="col-md-4 col-12 pl-md-0">
-                                    <div class="card mb-0">
-                                        <div class="card-header pb-50">
-                                            <h4 class="card-title">Sales History</h4>
-                                        </div>
-                                        <div class="card-content">
-                                            <div class="card-body py-1">
-                                                <div class="d-flex justify-content-between align-items-center mb-2">
-                                                    <div class="sales-item-name">
-                                                        <p class="mb-0">Airpods</p>
-                                                        <small class="text-muted">30 min ago</small>
-                                                    </div>
-                                                    <div class="sales-item-amount">
-                                                        <h6 class="mb-0"><span class="text-success">+</span> $50</h6>
-                                                    </div>
-                                                </div>
-                                                <div class="d-flex justify-content-between align-items-center mb-2">
-                                                    <div class="sales-item-name">
-                                                        <p class="mb-0">iPhone</p>
-                                                        <small class="text-muted">2 hour ago</small>
-                                                    </div>
-                                                    <div class="sales-item-amount">
-                                                        <h6 class="mb-0"><span class="text-danger">-</span> $59</h6>
-                                                    </div>
-                                                </div>
-                                                <div class="d-flex justify-content-between align-items-center">
-                                                    <div class="sales-item-name">
-                                                        <p class="mb-0">Macbook</p>
-                                                        <small class="text-muted">1 day ago</small>
-                                                    </div>
-                                                    <div class="sales-item-amount">
-                                                        <h6 class="mb-0"><span class="text-success">+</span> $12</h6>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="card-footer border-top pb-0">
-                                                <h5>Total Sales</h5>
-                                                <span class="text-primary text-bold-500">$82,950.96</span>
-                                                <div class="progress progress-bar-primary progress-sm my-50">
-                                                    <div class="progress-bar" role="progressbar" aria-valuenow="78" style="width:78%"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </section> --}}
-            <!-- Dashboard Ecommerce ends -->
+        <div class="content-body">
             <div class="row">
                 <div class="col-md-3">
                     <div class="card border text-center my-1">
@@ -156,14 +39,77 @@
                       </div>
                 </div>
                 <!-- Column Chart -->
-                <div class="col-lg-12 col-md-12">
+                <div class="col-lg-12 col-md-12 pt-5" id="monthlyChart">
                     <div class="card">
                     <div class="card-header">
-                        <h4 class="card-title">Column Chart</h4>
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="card-title">Monthly Chart</h4>
+                        <div class="form-group w-25">
+                            <label for="exampleFormControlSelect1">Select Year:</label>
+                            <?php  $current_year = date("Y"); $sel = request()->year;?>
+                            <select class="form-control" onchange="change_year(this.value)" id="exampleFormControlSelect1">
+                              @for ($i = $current_year; $i >= 2016; $i--)
+                                <option value="{{$i}}" {{$sel == $i ? 'selected' : null}}>{{$i}}</option>
+                              @endfor
+                               
+                            </select>
+                          </div>
+                        </div>
                     </div>
+                    @push('scripts')
+                       <script>
+                            function change_year(year){
+                                window.location.href = 'http://localhost/Flash/public/dashboard/'+year+'/1#monthlyChart'
+                            }
+                       </script>
+                    @endpush
                     <div class="card-content">
                         <div class="card-body">
                         <div id="column-chart"></div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <!-- Column Chart -->
+                <div class="col-lg-12 col-md-12 pt-5" id="weeklyChart">
+                    <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="card-title">Weekly Chart</h4>
+                        <div class="form-group w-25">
+                            <label for="selectYear">Select Year:</label>
+                            <?php  $current_year = date("Y"); $sel = request()->year;?>
+                            <select class="form-control" onchange="changing_both()" id="selectYear">
+                              @for ($i = $current_year; $i >= 2016; $i--)
+                                <option value="{{$i}}" {{$sel == $i ? 'selected' : null}}>{{$i}}</option>
+                              @endfor
+                               
+                            </select>
+                          </div>
+                        <div class="form-group w-25">
+                            <label for="selectMonth">Select month:</label>
+                            <?php  $current_month = date("m"); $selec = request()->month;?>
+                            <select class="form-control" onchange="changing_both()" id="selectMonth">
+                              @for ($j = 1; $j <= 12; $j++)
+                                <option value="{{$j}}" {{$selec == $j ? 'selected' : null}}>{{date("M", $j)}}</option>
+                              @endfor
+                               
+                            </select>
+                          </div>
+                        </div>
+                    </div>
+                    @push('scripts')
+                       <script>
+                            function changing_both(){
+                                var month = $("#selectMonth").val();
+                                var year = $("#selectYear").val();
+                                window.location.href = 'http://localhost/Flash/public/dashboard/'+year+'/'+month+'#weeklyChart'
+                            }
+                       </script>
+                    @endpush
+                    <div class="card-content">
+                        <div class="card-body">
+                        <div id="weekly-chart"></div>
                         </div>
                     </div>
                     </div>
@@ -229,6 +175,50 @@
         }
     };
     new ApexCharts(document.querySelector("#column-chart"), r).render();
+
+</script>
+
+
+<script>
+    
+    var e = "#5A8DEE",
+    t = [e, "#FDAC41", "#FF5B5C", "#39DA8A", "#00CFDD"];
+    var w = {
+    chart: { height: 350, type: "bar" },
+    colors: t,
+    plotOptions: {
+        bar: { horizontal: !1, endingShape: "rounded", columnWidth: "55%" }
+    },
+    dataLabels: { enabled: !1 },
+    stroke: { show: !0, width: 2, colors: ["transparent"] },
+    series: [
+        // { name: "Net Profit", data: [44, 55, 57, 56, 61, 58, 63, 60, 66] },
+        // { name: "Revenue", data: [76, 85, 101, 98, 87, 105, 91, 114, 94] },
+        {
+            name: "Total Sales Amount",
+            data: {{json_encode($weekly_sales)}}
+        }
+    ],
+    legend: { offsetY: -10 },
+    xaxis: {
+        categories: [
+            "week 1",
+            "week 2",
+            "week 3",
+            "week 4",
+        ]
+    },
+    yaxis: { title: { text: " Thousands" } },
+    fill: { opacity: 1 },
+    tooltip: {
+        y: {
+            formatter: function(e) {
+                return "Rs. " + e ;
+            }
+        }
+    }
+};
+new ApexCharts(document.querySelector("#weekly-chart"), w).render();
 
 </script>
 @endpush
